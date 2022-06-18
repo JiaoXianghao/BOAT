314eda7975a089ce341ecc7f61bcde780bab5275
JiaoXianghao <50295069+JiaoXianghao@users.noreply.github.com>
2022-06-18 16:26:17 +0800

    Add files via upload

--- test_script/test_system_feature.py
+++ test_script/test_system_feature.py
@@ -63,56 +63,7 @@ class TestSystemFeature:
             val_loss, forward_time, backward_time = optimizer.run_iter(tr_xs, tr_ys, tst_xs, tst_ys,
                                                                        iter, forward_with_whole_batch=False)
 
-    @pytest.mark.parametrize(
-        "method, ll_method, ul_method, ll_objective, ul_objective, truncate_iter,"
-        "truncate_max_loss_iter, alpha_init, alpha_decay, update_ll_model_init",
-        [
-            ("Feature", "Dynamic", "Recurrence", ll_o, ul_o, 0, False, 0.0, 0.0, False),
-            ("Feature", "Dynamic", "Recurrence", ll_o, ul_o, 0, True, 0.0, 0.0, False),
-            ("Feature", "Dynamic", "Recurrence", ll_o, ul_o, 0, False, 0.0, 0.0, True),
-            ("Feature", "Dynamic", "Recurrence", ll_o, ul_o, 0, True, 0.0, 0.0, True),
-            ("Feature", "Dynamic", "Recurrence", ll_o, ul_o, 1, False, 0.0, 0.0, False),
-            ("Feature", "Dynamic", "Recurrence", ll_o, ul_o, 1, False, 0.0, 0.0, True),
-            ("Feature", "Dynamic", "Recurrence", ll_o, ul_o, 0, False, 0.1, 0.5, False),
-            ("Feature", "Dynamic", "Recurrence", ll_o, ul_o, 0, True, 0.1, 0.5, False),
-            ("Feature", "Dynamic", "Recurrence", ll_o, ul_o, 0, False, 0.1, 0.5, True),
-            ("Feature", "Dynamic", "Recurrence", ll_o, ul_o, 0, True, 0.1, 0.5, True),
-            ("Feature", "Dynamic", "Onestage", ll_o, ul_o, 0, False, 0.0, 0.0, False),
-            ("Feature", "Dynamic", "Onestage", ll_o, ul_o, 0, False, 0.0, 0.0, True),
-            ("Feature", "Implicit", "LS", ll_o, ul_o, 0, False, 0.0, 0.0, False),
-            ("Feature", "Implicit", "NS", ll_o, ul_o, 0, False, 0.0, 0.0, False),
-            ("Feature", "Implicit", "GN", ll_o, ul_o, 0, False, 0.0, 0.0, False),
-            ("Feature", "BVFIM", "BVFIM", ll_o, ul_o, 0, False, 0.0, 0.0, False)
-        ],
-    )
-    def test_few_shot_res(self, method, ll_method, ul_method, ll_objective, ul_objective,
-                           truncate_iter, truncate_max_loss_iter, alpha_init, alpha_decay,
-                           update_ll_model_init):
-        ul_model = bolv.utils.model.backbone.Res12([5, 3, 84, 84], 5, use_head=False)
-
-        ll_model = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(np.prod(ul_model.output_shape[1:]), 5)
-        )
-        ll_opt = torch.optim.SGD(ll_model.parameters(), lr=0.1)
-        ul_opt = torch.optim.Adam(ul_model.parameters(), eps=1e-5)
-        tr_xs = torch.rand(4, 5, 3, 84, 84)
-        tr_ys = torch.tensor([0, 1, 2, 3, 4]).repeat(4, 1)
-        tst_xs = torch.rand(4, 75, 3, 84, 84)
-        tst_ys = torch.tensor([0, 1, 2, 3, 4]).repeat(4, 15)
-
-        optimizer = bolv.optimizer.BOLVOptimizer(
-            method, ll_method=ll_method, ul_method=ul_method,
-            ll_objective=ll_o, ul_objective=ul_o,
-            ll_model=ll_model, ul_model=ul_model)
-        optimizer.build_ll_solver(1 if ul_method == 'Onestage' else 5, ll_opt, truncate_iter=truncate_iter,
-                                  alpha_init=alpha_init, alpha_decay=alpha_decay)
-        optimizer.build_ul_solver(ul_opt, update_ll_model_init)
-
-        for iter in range(5):
-            val_loss, forward_time, backward_time = optimizer.run_iter(tr_xs, tr_ys, tst_xs, tst_ys,
-                                                                       iter, forward_with_whole_batch=False)
-
+    
     @pytest.mark.parametrize(
         "method, ll_method, ul_method, ll_objective, ul_objective, truncate_iter,"
         "truncate_max_loss_iter, alpha_init, alpha_decay, update_ll_model_init",
